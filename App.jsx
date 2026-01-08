2b8f9757951794118d384eb88220f4e87b0a29fe
ladlyghosh-prog <ladlyghosh@gmail.com>
2026-01-08 14:30:43 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="landing-page">
+      <h1>Paradise Nursery</h1>
+      <p>Your one-stop shop for beautiful plants.</p>
+      <button className="get-started-btn">Get Started</button>
+    </div>
+  );
+}
+
+export default App;
